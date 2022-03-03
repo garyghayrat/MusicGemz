85779252a309742221fe98ca27a00e2e5ce7d823
Gary Ghayrat <f255eb47-1441-48a5-be8e-0a9e3193245c@anonaddy.me>
2022-03-03 00:09:25 -0500

    contract first draft uploaded to mumbai

--- contracts/Gemz.sol
+++ contracts/Gemz.sol
@@ -1,17 +1,15 @@
 //SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "hardhat/console.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Gemz {
+contract Gemz is Ownable {
 
-  address owner;
   uint public fileCount;
   
 
-  constructor() {
-    owner = msg.sender;   
+  constructor() Ownable() {
     fileCount = 0;
   }
 
@@ -20,44 +18,40 @@ contract Gemz {
   struct File {
     uint fileID;
     string fileHash;
+    string coverHash;
     string fileName;
     string artistName;
     address payable artistAddr;
     string genre;
-    Donor[] donors;
+    address[] donors;
     mapping(address => Liked) liked;
     uint likesCount;
-  }
-
-  struct Donor {
-    address addr;
-    bool liked;
   }
 
   event Uploaded (address _artist, string _fileHash, uint _fileID);
   event Donate (address _artistAddr, uint _amount, address _donorAddr);
 
   File[] public files;
-  mapping(address => uint) public balances;
+  // mapping(address => uint) public balances;
 
   function uploadFile(
-    string memory _fileHash, 
+    string memory _fileHash,
+    string memory _coverHash, 
     string memory _fileName, 
     string memory _artistName, 
     string memory _genre) public {
 
     require(bytes(_fileHash).length > 0, "File hash is not valid");
 
-    Donor memory artist = Donor(msg.sender, true);
-
     File storage file = files.push();
 
-    file.fileID = fileCount;
+    file.fileID = fileCount+1;
     file.fileHash = _fileHash;
+    file.coverHash = _coverHash;
     file.fileName = _fileName;
     file.artistName = _artistName;
     file.genre = _genre;
-    file.donors.push(artist);
+    file.donors.push(msg.sender);
     file.liked[msg.sender] = Liked.Yes;
 
     fileCount++;
@@ -70,19 +64,22 @@ contract Gemz {
     address payable artist = files[id].artistAddr;
     require(msg.sender != artist, "You cannot donate to yourself");
 
-    balances[artist] += msg.value;
+    (bool sent, ) = artist.call{value: msg.value}("");
+    require(sent, "failed to send Ether");
+
+    // balances[artist] += msg.value;
     files[id].likesCount++;
 
     emit Donate(artist, msg.value, msg.sender);
   }
 
-  function withdraw() public payable {
-    uint balance = balances[msg.sender];
-    require(balance > 0);
-    balances[msg.sender] = 0;
+  // function withdraw() public payable {
+  //   uint balance = balances[msg.sender];
+  //   require(balance > 0);
+  //   balances[msg.sender] = 0;
 
-    (bool sent, ) = msg.sender.call{value: balance}("");
-    require(sent, "failed to send Ether");
-  }
+  //   (bool sent, ) = msg.sender.call{value: balance}("");
+  //   require(sent, "failed to send Ether");
+  // }
 
 }
